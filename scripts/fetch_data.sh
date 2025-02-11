4ad7011b63860f9981d1febedbc5a8f89e4a8375
chiranjivi <chiranjivikeshav1@gmail.com>
2025-02-11 15:01:21 +0530

    project setup

---
+++ scripts/fetch_data.sh
@@ -0,0 +1,1 @@
+(Invoke-WebRequest -Uri "https://www.deribit.com/api/v2/public/get_tradingview_chart_data?instrument_name=BTC-PERPETUAL&resolution=1&start_timestamp=1695000000000&end_timestamp=1705000000000" -Method Get).Content | Out-File "deribit_data.json" 
