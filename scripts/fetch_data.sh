f04f28610387bf51ed83ced963bcb7fa7116a45a
chiranjivi <chiranjivikeshav1@gmail.com>
2025-02-24 00:36:44 +0530

    training and testing using lstm

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,4 +1,4 @@
 # Actual trade data for BTC-PERPETUAL
 (Invoke-WebRequest -Uri "https://www.deribit.com/api/v2/public/get_tradingview_chart_data?instrument_name=BTC-PERPETUAL&resolution=1&start_timestamp=1695000000000&end_timestamp=1705000000000" -Method Get).Content | Out-File "deribit_data.json" 
 # Test trade data for BTC-PERPETUAL
-(Invoke-WebRequest -Uri "https://test.deribit.com/api/v2/public/get_tradingview_chart_data?instrument_name=BTC-PERPETUAL&resolution=1&start_timestamp=1695000000000&end_timestamp=1705000000000" -Method Get).Content | Out-File "test_deribit_data.json"
+(Invoke-WebRequest -Uri "https://test.deribit.com/api/v2/public/get_tradingview_chart_data?instrument_name=BTC-PERPETUAL&resolution=1&start_timestamp=1695000000000&end_timestamp=1705000000000" -Method Get).Content | Out-File "train_deribit_data.json"
