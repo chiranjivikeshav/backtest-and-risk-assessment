51bebb2be1c9924f71af964783107b5283c5e03d
chiranjivi <chiranjivikeshav1@gmail.com>
2025-02-25 14:46:18 +0530

    Custom libraries updated

--- custom_libraries/scaler.py
+++ custom_libraries/scaler.py
@@ -1,15 +1,27 @@
+import numpy as np
+
 class MinMaxScalerCustom:
     def __init__(self, feature_range=(0, 1)):
         self.min = None
         self.max = None
-        self.range_min, self.range_max = feature_range
+        self.feature_range = feature_range  # Default range is (0,1)
 
     def fit(self, data):
-        self.min = min(data)
-        self.max = max(data)
+        """Compute the min and max values for scaling."""
+        # Minimum value for each feature
+        self.min = np.min(data, axis=0)  
+        # Maximum value for each feature
+        self.max = np.max(data, axis=0)  
 
     def transform(self, data):
-        return [(x - self.min) / (self.max - self.min) * (self.range_max - self.range_min) + self.range_min for x in data]
+        """Apply the min-max scaling transformation."""
+        if self.min is None or self.max is None:
+            raise ValueError("Scaler has not been fitted. Call fit() before transform().")
 
-    def inverse_transform(self, data):
-        return [(x - self.range_min) / (self.range_max - self.range_min) * (self.max - self.min) + self.min for x in data]
+        data_scaled = (data - self.min) / (self.max - self.min)
+        return data_scaled * (self.feature_range[1] - self.feature_range[0]) + self.feature_range[0]
+
+    def fit_transform(self, data):
+        """Compute min and max, then transform the data."""
+        self.fit(data)
+        return self.transform(data)
